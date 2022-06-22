77df29ea4d29c7047ec556e0f642eb08cf295364
weibik <j-szkola@wp.pl>
2022-06-22 17:40:02 +0200

    font experiments

--- expense_tracker.py
+++ expense_tracker.py
@@ -17,15 +17,20 @@ import sys
 
 def window():
     app = QApplication(sys.argv)
+    app.setStyleSheet("QLabel{font-size: 10pt;}")
     win = QMainWindow()
     win.setGeometry(960, 540, 600, 400)
     win.setWindowTitle("expense tracker")
 
     label = QtWidgets.QLabel(win)
     label.setText("Total spendings:")
-    label.move(100, 100)
+    label.move(250, 0)
 
     win.show()
     sys.exit(app.exec_())
+
+
+
+
 
 window()
